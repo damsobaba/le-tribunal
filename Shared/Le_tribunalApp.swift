ca8ed72da8dbc9920bc02b5154471a650ea41c26
Adam <adam.mabrouki@webwag.com>
2022-10-23 18:40:29 +0200

    Initial Commit

---
+++ Shared/Le_tribunalApp.swift
@@ -0,0 +1,17 @@
+//
+//  Le_tribunalApp.swift
+//  Shared
+//
+//  Created by Adam Mabrouki on 23/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct Le_tribunalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
